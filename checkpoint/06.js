1a36abc260a931744a5caddeeb3fca4af42321d4
Francisco Aleman <franciscoaleman1993@gmail.com>
2022-09-03 10:20:46 -0300

    first commit

---
+++ checkpoint/06.js
@@ -0,0 +1,27 @@
+//
+// 6️⃣ ***** EJERCICIO 6 ***** - sortPrimeHouses() 6️⃣
+// 
+// Implementar un algoritmo de ordenamiento, que además de ordenar un array de menor a mayor,
+// retorne false si un número dentro del array no es primo
+// EJEMPLOS:
+// Dado el siguiente array:
+// [25, 3, 6, 8, 5, 12, 9, 18, 11, 7]
+// sortPrimeHouses() retorna => false (porque 25 por ejemplo, no es primo)
+//
+// Dado este otro array:
+// [61, 7, 13, 11, 29, 3]
+// sortPrimeHouses() retorna => [3, 7, 11, 13, 29, 61]
+//⚠️ ATENCION ⚠️
+// NO utilizar el método sort() de Array!
+// REQUISITOS:
+//  🟢 Aplicar un algoritmo de ordenamiento de menor a mayor
+//  🟢 Si numuero dentro del array no es primo, retornar false
+
+function sortPrimeHouses(array) {
+  // Tu código aquí:
+}
+
+// ⚠️ NO MODIFICAR NADA POR DEBAJO DE ESTA LÍNEA ⚠️
+module.exports = {
+  sortPrimeHouses
+};
---
+++ checkpoint/08.js
@@ -0,0 +1,36 @@
+const { LinkedList } = require("../DS");
+// ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️
+//
+// 8️⃣ ***** EJERCICIO 8 ***** - LinkedList.sliceRooms() 8️⃣ 
+// 
+// Agregar al prototype de LinkedList el método sliceRooms().
+// Este método también será similar al slice() de array, dónde a partir de 2 índices, deberás
+// particionar la lista de habitaciones de una casa.
+//
+//⚠️ ATENCION ⚠️
+// - Documentación explicando Array.slice() --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// - Los valores que sean iguales a los índices no deben ser incluidos
+// - Los indices siempre van a equivaler a un string con el nombre de una habitación
+// - Recorda que esta el metodo add() ya incluido en el prototype para agregar nuevos nodos y search() para buscar los existentes
+//
+// EJEMPLOS:
+// Tenemos la siguiente lista:
+// ("Cocina") ---> ("Dormitorio") ---> ("Baño") ---> ("Living") ---> ("Garage") --->
+//
+// LinkedList.sliceRooms("Dormitorio", "Garage") Devuelve => ("Baño") ---> ("Living") --->
+//
+// REQUISITOS:
+//  🟢 Retornar una nueva lista a partir de los índices provistos
+//  🟢 Los valores que coincidan con los indices no deben ser incluidos
+//  🟢 Si ninguno de los índices se encuentra en la lista, retornar false
+//
+// Nota: el primer indice siempre sera menor al segundo.
+
+LinkedList.prototype.sliceRooms = function (firstIndex, secondIndex) {
+  // Tu código aquí:
+}
+
+// ⚠️ NO MODIFICAR NADA POR DEBAJO DE ESTA LÍNEA ⚠️
+module.exports = {
+  LinkedList
+};
